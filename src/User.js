106b09d774e9106b4ef82c40388fb53114021243
Sourav-k4 <cash.souravkr12@gmail.com>
2023-07-29 20:00:21 +0530

    groww

---
+++ src/User.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './User.css';
+export default function User(){
+    return(
+        <>
+                <div className='sidebar'>
+                    <div className='logo'>
+                    <h2>Sociogram</h2>
+                    </div>
+                            <div className='side-options'>
+                                <ul>
+                                    <li>Home</li>
+                                    <li>Search</li>
+                                    <li>Explore</li>
+                                    <li>Reels</li>
+                                    <li>Message</li>
+                                    <li>Notification</li>
+                                </ul>
+                            </div>
+                    </div>
+            
+            <div className='mainContainer'>
+                <div className='profile-section'>
+                
+                <div className='profile-pic'>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                </div>
+
+                <div className='profile-detail'>
+                    <h3>User Name</h3>
+                    <div>
+                        <span>23 Posts</span>
+                        <span>444 Followers</span>
+                        <span>222 Following</span>
+                    </div>
+                    <div>
+                        <p>
+                            The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela. The way to get started is to quit talking and begin doing.
+                        </p>
+                    </div>
+                </div>  
+            </div>
+                <div className='allposts'>
+                    <div >
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                    <div>
+                    <img src='https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'></img>
+                    </div>
+                </div>
+         </div>
+            
+        </>
+    );
+}
